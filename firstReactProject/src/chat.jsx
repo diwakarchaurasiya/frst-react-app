9ea0a04cfa6cae64f593ab6d127d551e268e3d2c
diwakar chaurasiya <diwakarbmj1212@gmail.com>
2023-10-05 19:58:46 +0530

    made changes in chat.js

--- firstReactProject/src/chat.jsx
+++ firstReactProject/src/chat.jsx
@@ -1,7 +1,9 @@
 function Chat() {
     
-return <>
-        <p>How Can you got it ?</p>
+    return <>
+        <p>How Can you got it ?</p> 
+        <button >Click ME</button>
+        <a href="https://www.linkedin.com/in/diwakar-chaurasiya-81b08a242//">Visit Our Site</a>
        </>
 }
 export default Chat
